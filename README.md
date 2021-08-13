70199bd078c234ebcef3067a7eb143ed34583c8d
Noachh <31162503+Noachh@users.noreply.github.com>
2021-08-14 00:22:50 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+<div class="animated_rainbow_1">Noach's Furby Control Panel</div>
+
+## [Take control](https:/Noachh.github.io/furble.html) 
---
+++ dlc/index.js
@@ -0,0 +1,31 @@
+[
+    { 
+        "file": "Sabaton.dlc", 
+        "title": "SABATON",
+        "buttons": [
+            { "title": "Sabaton", "action": [75,0,4,4] }
+        ]
+    },
+    { 
+        "file": "hacked.dlc", 
+        "title": "HACKED",
+        "buttons": [
+            { "title": "Hacked 1", "action": [75,0,3,4] },
+            { "title": "Hacked 2", "action": [75,0,4,4] }
+        ]
+    },
+    {
+        "file": "noimg2.dlc", 
+        "title": "Audio only",
+        "buttons": [
+            { "title": "Audio test ", "action": [75,0,4,4] }
+        ]
+    },
+    {
+        "file": "chilli.dlc", 
+        "title": "Chilli",
+        "buttons": [
+            { "title": "Chilli ", "action": [75,0,4,4] }
+        ]
+    }
+]
